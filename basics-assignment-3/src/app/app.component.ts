0c930ede68024ebd1760ca2cc47145472efd72f9
Ramon Custers <ramon.custers@gmail.com>
2018-09-10 17:53:07 +0200

    1810090552

--- basics-assignment-3/src/app/app.component.ts
+++ basics-assignment-3/src/app/app.component.ts
@@ -8,30 +8,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   displayYesNo = false;
-  clicksArray =[
-    {number: 0,
-    timeStamp: Date()}
-  ];
-  clickNo = 0;
+  clicksArray = [];
   dateTime;
   
   displayDetails() {
-    this.clickNo = this.clickNo + 1;
-    this.dateTime = new Date();
     this.displayYesNo = this.displayYesNo ? false : true;
-    this.clicksArray.push(  {number: this.clickNo, timeStamp: this.dateTime} );
-  };
-
-  getBackGroundColor(number) {
-    if ( number > 4 ) {
-      return 'blue';
-    }
-  };
-
-  getTextColor(number) {
-    if ( number > 4 ) {
-      return 'white';
-    }
+    this.clicksArray.push(new Date());
   };
 
 }
