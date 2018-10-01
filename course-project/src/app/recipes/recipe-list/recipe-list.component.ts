208464b64e34c3b69445be61d6a24096aef6cf02
Ramon Custers <ramon.custers@gmail.com>
2018-10-01 18:59:33 +0200

    commit 201810011859

--- course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
 
 @Component( {
     selector: 'app-recipe-list',
     templateUrl: './recipe-list.component.html'
 } )
 
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+    recipes: Recipe[] = [
+        new Recipe('A Test Recipe', 'This is simply a test', '../recipe-list/logo.png'),
+        new Recipe('A Test Recipe', 'This is simply a test', '../recipe-list/logo.png'),
+        new Recipe('A Test Recipe', 'This is simply a test', '../recipe-list/logo.png')
+    ];
+
+    constructor() {}
+
+    ngOnInit() {}
 
 }
